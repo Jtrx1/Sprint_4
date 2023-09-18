b3b9298c28be5bdd852d28503992a7108c117941
Mikhail Tuganov <jtrx1@yandex.ru>
2023-09-18 22:53:25 +0300

    Изменил, параметризировал тест ChecksTextAccordionPanelTest

--- src/test/java/ChecksTextAccordionPanelTest.java
+++ src/test/java/ChecksTextAccordionPanelTest.java
@@ -1,19 +1,32 @@
 import org.example.pageobject.MainPage;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import java.util.Arrays;
 
+import static org.example.Settings.*;
 import static org.junit.Assert.assertEquals;
-import static org.example.Settings.ACCORDION_PANEL_STRINGS;
 
-
+@RunWith(Parameterized.class)
 public class ChecksTextAccordionPanelTest extends BaseTest{
+    private final String accordionPanelString;
+
+    public ChecksTextAccordionPanelTest(String accordionPanelString){
+        this.accordionPanelString=accordionPanelString;
+    }
+    @Parameterized.Parameters
+    public static String[] getTestString() {
+        return ACCORDION_PANEL_STRINGS;
+    }
+
     @Test
     public void accordionPanelTest(){
-        for (int i = 0; i < 7; i++) {
-            MainPage objPage=new MainPage(driver);
-            objPage.clickAccordionHeading(objPage.getAccordionHeading(), i);
-                    assertEquals("Ошибка при проверке панели вопросов: ",ACCORDION_PANEL_STRINGS[i],
-                            driver.findElement(By.xpath(String.format("//div[@id='accordion__panel-%d']/p", i))).getText());
-        }
+        System.out.println(accordionPanelString);
+        int i = Arrays.asList(ACCORDION_PANEL_STRINGS).indexOf(accordionPanelString);
+        MainPage objPage=new MainPage(driver);
+        objPage.clickAccordionHeading(objPage.getAccordionHeading(), i);
+        assertEquals("Ошибка при проверке панели вопросов: ", accordionPanelString,
+               driver.findElement(By.xpath(String.format("//div[@id='accordion__panel-%d']/p", i))).getText());
     }
 }
